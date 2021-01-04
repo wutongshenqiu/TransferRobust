4d934977013fdd3e1bef59ad64064be0a2831208
imTyrant <cd2235823998@gmail.com>
2021-01-04 17:13:25 +0800

    experiment stuffs added by imTyrant

--- scripts/pwrn34_neft_sn_norm.sh
+++ scripts/pwrn34_neft_sn_norm.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ###############################################
 # check CUDA_VISIBLE_DEVICES is set or not
 if [[ -z ${CUDA_VISIBLE_DEVICES} ]]; then
@@ -22,31 +23,33 @@ function valid () {
 
 MODEL_PATH=trained_models
 POWER_ITER=1
-NORM_BETA=0.6
-FREEZE_BN_LAYER=true
+NORM_BETA=0.4
+FREEZE_BN_LAYER=True
 
-for k in 4 6 8; do
-    TEACHER_MODEL=cartl_wrn34_cifar100_${k}_0.01-best_robust
+for lambda in 0.01; do
+    for k in 4 6 8; do
+        TEACHER_MODEL=cartl_wrn34_cifar100_${k}_${lambda}-best_robust
 
-    echo "#######################################################"
-    echo "using NEFT(SN) to transfer from ${TEACHER_MODEL}"
-    python -m exps.neft_spectrum_norm --model pwrn34 --num_classes 10 --dataset cifar10 --teacher ${TEACHER_MODEL} -k ${k} \
-             --power-iter ${POWER_ITER} --norm-beta ${NORM_BETA} --freeze-bn ${FREEZE_BN_LAYER}
-    valid
+        if [ ! -e "${MODEL_PATH}/${TEACHER_MODEL}" ]; then
+            echo "CANNOT FIND ${MODEL_PATH}/${TEACHER_MODEL}, SKIPPING"
+            continue
+        fi
 
-    python -m exps.eval_robust_pwrn34 --model=${MODEL_PATH}/sntl_${POWER_ITER}_${NORM_BETA}_pwrn34_cifar10_${k}_${TEACHER_MODEL}-last -k=${k} \
-            --log=sntl.log --result-file=logs/sntl.json
-    valid
+        if [ ${FREEZE_BN_LAYER} = "True" ]; then
+            freeze_bn_op="--freeze-bn"
+        else
+            freeze_bn_op=""
+        fi
 
+        echo "#######################################################"
+        echo "using NEFT(SN) to transfer from ${TEACHER_MODEL}"
+        python -m exps.neft_spectrum_norm --model=pwrn34 --num_classes=10 --dataset=cifar10 --teacher=${TEACHER_MODEL} -k=${k} \
+                --power-iter=${POWER_ITER} --norm-beta=${NORM_BETA} ${freeze_bn_op}
+        valid
+
+        python -m exps.eval_robust_pwrn34 --model=${MODEL_PATH}/sntl_${POWER_ITER}_${NORM_BETA}_${FREEZE_BN_LAYER}_pwrn34_cifar10_${k}_${TEACHER_MODEL}-last -k=${k} \
+                --log=sntl.log --result-file=logs/sntl.json
+        valid
+
+    done
 done
-
-TEACHER_MODEL=cartl_wrn34_cifar100_8_1.0-best_robust
-echo "#######################################################"
-echo "using NEFT(SN) to transfer from ${TEACHER_MODEL}"
-python -m exps.neft_spectrum_norm --model pwrn34 --num_classes 10 --dataset cifar10 --teacher ${TEACHER_MODEL} -k 8\
-        --power-iter ${POWER_ITER} --norm-beta ${NORM_BETA} --freeze-bn ${FREEZE_BN_LAYER}
-valid
-python -m exps.eval_robust_pwrn34 --model=${MODEL_PATH}/sntl_${POWER_ITER}_${NORM_BETA}_pwrn34_cifar10_8_${TEACHER_MODEL}-last -k=8 \
-        --log=sntl.log --result-file=logs/sntl.json
-valid
-
